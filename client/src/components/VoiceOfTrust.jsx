753a2c0daab7351a35a4086aa3867612e2b26465
Mr. AKS <47912137+AKSKumar2002@users.noreply.github.com>
2025-07-21 12:59:24 +0530

    ss

--- client/src/components/VoiceOfTrust.jsx
+++ client/src/components/VoiceOfTrust.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const testimonials = [
   {
     name: "Avvaiyar",
-    image:"/src/assets/Avvaiyar.png",
+    image:"https://chintan.indiafoundation.in/wp-content/uploads/2021/07/Avvaiyar-.png",
     quote:
       '"Food itself gives the body health". Avvaiyar\'s wisdom in her poems often emphasises the importance of food and its role in sustaining life and health, which could easily include vegetables.',
   },
   {
     name: "Kannadasan",
-    image:"/src/assets/kannadasan.jpg",
+    image:"https://i.pinimg.com/564x/10/27/8c/10278c5907525cffdefe283bb7947f0c.jpg",
     quote:
       '"The right amount of food give us life". This could be interpreted as a message about importance of moderation and the nourishment that comes from wholesome foods, such as vegetables.',
   },
   {
     name: "Bharathiyar",
-    image:"/src/assets/bharathiyar logo.jpg",
+    image:"https://i.pinimg.com/736x/17/6d/44/176d4458c664ff37c5ae1cdede75b42d.jpg",
     quote:
       '"When food, body, and knowledge are in harmony, life becomes prosperous". While not directly about vegetables, this quote emphasises the importance of balanced food, which is integral to our health and well-being.',
   },
