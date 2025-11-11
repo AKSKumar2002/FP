989f9263d9db2c1d4d94e5c90d36eeb5b5f3e048
Mr. AKS <47912137+AKSKumar2002@users.noreply.github.com>
2025-11-11 15:48:49 +0530

    32

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -76,7 +76,7 @@ const BestSeller = () => {
     return (
         <>
             <div className='mt-16'>
-                <p className='text-2xl md:text-3xl font-medium'>Daily Freshies</p>
+                <p className='text-2xl md:text-3xl font-medium'></p>
                 <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
                     {products.filter((product) => product.inStock).slice(0, 5).map((product, index) => (
                         <div
